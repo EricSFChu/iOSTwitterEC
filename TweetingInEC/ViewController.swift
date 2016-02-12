64fe5a6f585de04c9ebcb8b0a9dad67df87cbd44
EricSFChu <echu6@mail.ccsf.edu>
2016-02-11 23:24:47 -0800

    Added Tweet and User Classes Added class variable currentUser

--- TweetingInEC/ViewController.swift
+++ TweetingInEC/ViewController.swift
@@ -23,18 +23,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onLogin(sender: AnyObject) {
-        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
-        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "tweetinginec://oauth"), scope: nil, success: {
-            (requestToken: BDBOAuth1Credential!) -> Void in
-                print("Got the request token")
-            let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
-            UIApplication.sharedApplication().openURL(authURL!)
-
-            })
-            {(error: NSError!) -> Void in
-                    print("Failed to get request token")
+        TwitterClient.sharedInstance.loginWithCompletion() {
+            (user: User?, error: NSError?) in
+            if user != nil {
+                // perform segue
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            } else {
+                // handle login error
+            }
         }
-        
         
     }
 
