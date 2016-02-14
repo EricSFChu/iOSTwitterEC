03fc079157986494e68b096f649da01b6221f7e8
EricSFChu <echu6@mail.ccsf.edu>
2016-02-14 02:06:48 -0800

    Implemented Retweet.

--- Readme.md
+++ Readme.md
@@ -9,8 +9,8 @@ Time spent: **3** hours spent in total
 The following **required** functionality is completed:
 
 - [x] User can sign in using OAuth login flow
-- [ ] User can view last 20 tweets from their home timeline
-- [ ] The current signed in user will be persisted across restarts
+- [x] User can view last 20 tweets from their home timeline
+- [x] The current signed in user will be persisted across restarts
 - [ ] In the home timeline, user can view tweet with the user profile picture, username, tweet text, and timestamp.
 - [ ] Retweeting and favoriting should increment the retweet and favorite count.
 
--- TweetingInEC/TwitterClient.swift
+++ TweetingInEC/TwitterClient.swift
@@ -83,4 +83,26 @@ class TwitterClient: BDBOAuth1SessionManager {
             self.loginCompletion?(user: nil, error: error)
                 }
     }
+    
+    func retweet(id: Int, params: NSDictionary?, completion: (error: NSError?) -> () ){
+        POST("1.1/statuses/retweet/\(id).json", parameters: params, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
+            print("Retweeted tweet with id: \(id)")
+            completion(error: nil)
+            }, failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
+                print("Couldn't retweet")
+                completion(error: error)
+            }
+        )
+    }
+    
+    func likeTweet(id: Int, params: NSDictionary?, completion: (error: NSError?) -> () ){
+        POST("1.1/favorites/create.json?id=\(id)", parameters: params, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
+            print("Liked tweet with id: \(id)")
+            completion(error: nil)
+            }, failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
+                print("Couldn't like tweet")
+                completion(error: error)
+            }
+        )}
+
 }
