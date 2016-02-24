4ca8f7b55b464083f337793bad93d6afb0809f24
EricSFChu <echu6@mail.ccsf.edu>
2016-02-24 02:20:55 -0800

    Added reply feature using same endpoint as tweet.

--- TweetingInEC/TwitterClient.swift
+++ TweetingInEC/TwitterClient.swift
@@ -138,5 +138,16 @@ class TwitterClient: BDBOAuth1SessionManager {
             }
         )
     }
+    
+    func reply(escapedTweet: String, statusID: Int, params: NSDictionary?, completion: (error: NSError?) -> () ){
+        POST("1.1/statuses/update.json?in_reply_to_status_id=\(statusID)&status=\(escapedTweet)", parameters: params, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
+            print("tweeted: \(escapedTweet)")
+            completion(error: nil)
+            }, failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
+                print("Couldn't tweet")
+                completion(error: error)
+            }
+        )
+    }
 
 }
